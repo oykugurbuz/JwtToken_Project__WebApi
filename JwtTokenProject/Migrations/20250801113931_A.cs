fb825f986fcd393715050c910ccf7543bb459054
oykugurbuz <oyku.gurbuz@outlook.com>
2025-08-01 16:56:06 +0300

    .

---
+++ JwtTokenProject/Migrations/20250801113931_A.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JwtTokenProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class A : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedAttempt",
+                table: "AppUserInfos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "AppUserInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "AppUserInfos",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RememberMe",
+                table: "AppUserInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "AppUserInfos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserTypeName",
+                table: "AppUserInfos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedAttempt",
+                table: "AppUserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "AppUserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "AppUserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "RememberMe",
+                table: "AppUserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "AppUserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "UserTypeName",
+                table: "AppUserInfos");
+        }
+    }
+}
